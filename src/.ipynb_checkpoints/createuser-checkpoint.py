545075952a5806e8907c5115bffd498654c45956
zubairkhan1234 <zubairsaleem1210@gmail.com>
2025-08-10 01:35:36 +0500

    just initialize project

---
+++ src/.ipynb_checkpoints/createuser-checkpoint.py
@@ -0,0 +1,10 @@
+def create_user():
+    name = input("Enter your Name");
+    email = input('Enter your Email');
+    address = input('Enter your complete Address')
+    country = input("Enter your Country Name")
+    city = input('Enter your City Name')
+    cnic = input("Enter your cnic Name")
+
+    return userData = {"name": name, "email": email, "address": address, "country": country, "city": city, "cnic": cnic}
+    
