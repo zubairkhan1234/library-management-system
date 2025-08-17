29821fc74caa598bef89005c08c3851a1a41311c
zubairkhan1234 <zubairsaleem1210@gmail.com>
2025-08-17 20:15:08 +0500

    feat(library): add search_by_title method and improve search_by_author
    - Added `search_by_title` method to allow searching books by title.
    - Enhanced `search_by_author` to display count, handle not found case,
      and print structured search results.
      - Added `availabe_books` method to list all available books.
      - Updated `index.ipynb` to initialize sample book data and test new
        methods.

--- src/.ipynb_checkpoints/createbook-checkpoint.py
+++ src/.ipynb_checkpoints/createbook-checkpoint.py
@@ -23,18 +23,39 @@ class libraryModule:
 
         return self.books
 
-    def search_by_author(self):
-        author_name = input('type author name to search books')
-
-        for book in self.books:
-            if author_name.lower() in book['author'].lower():
-                print(book['author'])
-
-
-
     def search_by_title(self):
-        author_name = input('type author name to search books')
+        title_name = input('type author name to search books')
 
+        for book in self.books:
+            if title_name.lower() in book['title'].lower():
+                print(book['title'])
+
+
+
+    def search_by_author(self):
+        author_name = input('Type author name to search books: ')
+        count = 0
+        search_result = []
         for book in self.books:
             if author_name.lower() in book['author'].lower():
                 print(book['author'])
+                search_result.append(book)
+                count = count + 1
+        if count > 0:
+            print(f"{count} book(s) were found")
+        if(count == 0 ):
+            print('Data not found')
+        print('=========++++++++++==============')
+        print(search_result)
+        
+    def availabe_books(self):
+        all_available_books = []
+        for book in self.books:
+            if (int(book["available_copies"])) > 0:
+            
+                all_available_books.append(book)
+        if len(all_available_books) > 0:
+            return all_available_books
+        else:
+            print('currently not available book')
+    # def borrow_a_book(self):
